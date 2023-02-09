c28f9535a69967d5e038bfe8a05ac28ec345b094
aziz-codes <azizisoffline@gmail.com>
2023-02-10 00:40:59 +0500

    List, ListItem,Switch, Icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,98 @@
 import React from "react";
-import { Box } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from "@mui/material";
+import {
+  Home,
+  Inbox,
+  Mail,
+  ModeNight,
+  Pages,
+  Pageview,
+  People,
+  PeopleAlt,
+  Person,
+  Settings,
+} from "@mui/icons-material";
 const Sidebar = () => {
   return (
     <Box
-      bgcolor="skyblue"
       flex={1}
       flexShrink="unset"
       maxWidth="350px"
       p={2}
       sx={{ display: { xs: "none", sm: "block" } }}
     >
-      Sidebar
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton LinkComponent="a" href="#home">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton LinkComponent="a" href="#groups">
+            <ListItemIcon>
+              <People />
+            </ListItemIcon>
+            <ListItemText primary="Groups" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton LinkComponent="a" href="#mails">
+            <ListItemIcon>
+              <Mail />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton LinkComponent="a" href="#pages">
+            <ListItemIcon>
+              <Pageview />
+            </ListItemIcon>
+            <ListItemText primary="Pages" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton LinkComponent="a" href="#settings">
+            <ListItemIcon>
+              <Settings />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton LinkComponent="a" href="#friend">
+            <ListItemIcon>
+              <Person />
+            </ListItemIcon>
+            <ListItemText primary="User" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton LinkComponent="a" href="#">
+            <ListItemIcon>
+              <ModeNight />
+            </ListItemIcon>
+            <Switch />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
